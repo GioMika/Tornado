f96eadaa0120551bab63392e3e577e78eb600dd8
mikadze giorgi <mikadzegio9000@gmail.com>
2025-02-03 20:47:22 +0400

    setings paths 1

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,12 +1,18 @@
 import classes from './Services.module.css';
 import updating from '../../images/updating.svg';
-import updating2 from '../../images/updating2.svg';
 import improvement from '../../images/improvement.svg';
 import rent from '../../images/rent.svg';
 import leasing from '../../images/leasing.svg';
 import Partners from "../Partners/Partners";
+import {useNavigate} from "react-router-dom";
 
 const Services = () => {
+	const navigate = useNavigate();
+	const handelNavigate = (path) => {
+		navigate(path)
+	}
+
+
 	return (
 		 <>
 			 <section className={classes.main_section}>
@@ -16,26 +22,27 @@ const Services = () => {
 				 </div>
 				 <div className={classes.blocks}>
 
-					 <div className={classes.block_updating}>
+					 <div onClick={() => handelNavigate('/subscription')}  className={classes.block_updating}>
 						 <div className={classes.title_updating}>
 							 <h2>Годовая подписка на программную модернизацию и мелкий ремонт</h2>
 						 </div>
 						 <img className={classes.img_updating} src={updating} alt=""/>
 					 </div>
 
-					 <div className={classes.block_leasing}>
+					 <div onClick={() => handelNavigate('/leasing')} className={classes.block_leasing}>
 						 <div className={classes.title_leasing}>
 							 <h2>Лизинг</h2>
 						 </div>
 						 <img className={classes.img_leasing} src={leasing} alt=""/>
 					 </div>
-					 <div className={classes.block_improvement}>
+					 <div onClick={() => handelNavigate('/equipping')} className={classes.block_improvement}>
 						 <div className={classes.title_improvement}>
 							 <h2>Дооснащение техники оборудованием клиента</h2>
+
 						 </div>
 						 <img className={classes.img_improvement} src={improvement} alt=""/>
 					 </div>
-					 <div className={classes.block_rent}>
+					 <div onClick={() => handelNavigate('/rent')} className={classes.block_rent}>
 						 <img className={classes.img_rent} src={rent} alt=""/>
 						 <div className={classes.title_rent}>
 							 <h2>Аренда техники</h2>
