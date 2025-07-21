96c8f2f234430c501c4aa4abee9ec005b5ee30aa
mikadze giorgi <mikadzegio9000@gmail.com>
2025-07-21 12:51:29 +0400

    test commit

--- src/pages/RobotsComplexPage/RobotsComplexPage.jsx
+++ src/pages/RobotsComplexPage/RobotsComplexPage.jsx
@@ -1,65 +1,71 @@
-import classes from './index.module.css'
-import Button from "../../helpers/Button/Button";
-import robot from "../../images/robot2.svg";
-import robot_2 from "../../images/robot_2.svg";
-import {useNavigate} from "react-router-dom";
+import classes from './index.module.css';
+import Button from '../../helpers/Button/Button';
+import robot from '../../images/robot2.svg';
+import { useNavigate } from 'react-router-dom';
+import img1 from './imgs/Sub-1.png'
+import img2 from './imgs/Sub-2.png'
+
+const robots = [
+	{
+		id: 1,
+		path: '/tornado',
+		title: 'Торнадо (колесно-шагающий)',
+		subTitle: 'Торнадо',
+		description: 'Колесно-шагающий мобильный робот малого класса с грузоподъемностью до 25кг.',
+		image: robot,
+	},
+	{
+		id: 2,
+		path: '/typhoon',
+		title: 'Тайфун (колесно-шагающий)',
+		subTitle: 'Тайфун',
+		description: 'Колесно-шагающий мобильный робот среднего класса с грузоподъемностью до 115кг.',
+		image: img1,
+	}
+];
 
 const RobotsComplexPage = () => {
 	const navigate = useNavigate();
-	const handelNavigate = (path) => {
-		navigate(path)
-	}
 
 	return (
 		 <div className={classes.container}>
 			 <div className={classes.main_title}>
-				 <h2 className={classes.h2}>Роботизированные <br/> комплексы</h2>
+				 <h2 className={classes.h2}>Роботизированные <br /> комплексы</h2>
 			 </div>
+
 			 <div className={classes.topBlocks}>
-
-				 <div onClick={() => handelNavigate('/tornado')} className={classes.block}>
-					 <div className={classes.textBlock}>
-						 <h2 className={classes.title_tornado}>Торнадо (колесно-шагающий)</h2>
-						 <div className={classes.paragraph}>
-							 <h2 className={classes.title_tornado2}>Торнадо</h2>
-							 <span>Колесно-шагающий мобильный робот малого класса с грузоподъемностью до 25кг.</span>
-						 </div>
-						 <Button className={classes.btns}>Подробнее</Button>
-					 </div>
-					 <div className={classes.imageBlock}>
-						 <img className={classes.robot} src={robot} alt="Placeholder"/>
-						 <img className={classes.robot_2} src={robot_2} alt="Placeholder"/>
-					 </div>
-				 </div>
-
-
-				 <div onClick={() => handelNavigate('/typhoon')} className={classes.block}>
-					 <div className={classes.textBlock}>
-						 <h2 className={classes.title_tornado}>Тайфун (колесно-шагающий)</h2>
-						 <div className={classes.para_robot}>
-							 <h2 className={classes.title_tornado2}>Тайфун</h2>
-							 <span>Колесно-шагающий мобильный робот среднего класса с грузоподъемностью до 115кг.</span>
-						 </div>
-						 <Button className={classes.btns}>Подробнее</Button>
-					 </div>
-					 <div className={classes.imageBlock}>
-						 <img className={classes.robot} src={robot} alt="Placeholder"/>
-						 <img className={classes.robot_2} src={robot_2} alt="Placeholder"/>
-					 </div>
-				 </div>
+				 {robots.map(({ id, path, title, subTitle, description, image }) => (
+						<div key={id} onClick={() => navigate(path)} className={classes.block}>
+							<div className={classes.textBlock}>
+								<h2 className={classes.title_tornado}>{title}</h2>
+								<div className={classes.paragraph}>
+									<h2 className={classes.title_tornado2}>{subTitle}</h2>
+									<span>{description}</span>
+								</div>
+								<Button className={classes.btns}>Подробнее</Button>
+							</div>
+							<div className={classes.imageBlock}>
+								<img className={classes.robot} src={image} alt={subTitle} />
+								<img className={classes.robot_2} src={image} alt={`${subTitle} 2`} />
+							</div>
+						</div>
+				 ))}
 			 </div>
 
 			 <div className={classes.fullWidthBlock}>
+				 <img src={img2} alt=""/>
 				 <div className={classes.textBlock}>
 					 <div className={classes.text}>
 						 <h3>Архивные модели</h3>
 						 <p>Эти модели роботов устарели и сняты с производства.</p>
-						 <li>Кобра</li>
-						 <li>Мангуст</li>
-						 <li>Верхолаз</li>
-						 <li>Варан</li>
-						 <li>Вездеход ТМ3</li>
-						 <li>УТК</li>
+						 <ul>
+							 <li>Кобра</li>
+							 <li>Мангуст</li>
+							 <li>Верхолаз</li>
+							 <li>Варан</li>
+							 <li>Вездеход ТМ3</li>
+							 <li>УТК</li>
+						 </ul>
 						 <Button className={classes.btn}>Подробнее</Button>
 					 </div>
 				 </div>
