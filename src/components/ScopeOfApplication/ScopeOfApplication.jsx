4d661ef0d788dd26039b8900d9edfe6f730fedf1
Olha Sotova <sotovaola21@gmail.com>
2025-07-30 17:20:56 +0200

    images in scope have been changed

--- src/components/ScopeOfApplication/ScopeOfApplication.jsx
+++ src/components/ScopeOfApplication/ScopeOfApplication.jsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from "react";
 import classes from "./styles.module.css";
 import Features from "../Features/Features";
+
 import background2 from "../../images/Mask group.svg";
 import mobileRobot from "../../images/mobile_robot.svg";
+
 import icons1 from "../../images/image1.svg";
 import icons2 from "../../images/image2.svg";
 import icons3 from "../../images/image3.svg";
@@ -12,95 +14,130 @@ import icons6 from "../../images/image6.svg";
 import icons7 from "../../images/image7.svg";
 import icons8 from "../../images/image8.svg";
 
+import image1 from "../../images/scope/_БДКГ.Final Color Output-01.png";
+import image2 from "../../images/scope/2-01.png";
+import image3 from "../../images/scope/3-01.png";
+import image4 from "../../images/scope/4-01.png";
+import image5 from "../../images/scope/5-01.png";
+import image6 from "../../images/scope/6-01.png";
+
+const images = [image1, image2, image3, image4, image5, image6, image6];
+
 const applications = [
   {
     id: 1,
     icon: icons1,
     title: "Разбор завалов",
-    description: "Роботы представляют из себя прочную конструкцию и не рискуют жизнью при работе в хлипких конструкциях"
+    description:
+      "Роботы представляют из себя прочную конструкцию и не рискуют жизнью при работе в хлипких конструкциях",
   },
   {
     id: 2,
     icon: icons2,
     title: "Визуальная/Радиационная/Инженерная разведка",
-    description: "Поворотные камеры с большим оптическим увеличением или тепловизионным изображением, различные типы датчиков и сенсоров позволяют адаптировать робота под любую задачу"
+    description:
+      "Поворотные камеры с большим оптическим увеличением или тепловизионным изображением, различные типы датчиков и сенсоров позволяют адаптировать робота под любую задачу",
   },
   {
     id: 3,
     icon: icons3,
     title: "Патруль местности",
-    description: "Способность гибкой модификации системы позволяет настроить робота под уникальные задачи вашей территории, автоматизировать или дистанцировать необходимые процессы."
+    description:
+      "Способность гибкой модификации системы позволяет настроить робота под уникальные задачи вашей территории, автоматизировать или дистанцировать необходимые процессы.",
   },
   {
     id: 4,
     icon: icons4,
     title: "Измерение содержания хим. веществ",
-    description: "У вас химическое предприятие с большой территорией? Запустите,оснащенного необходимым набором сенсоров, робота контролировать периметр и он выполнит подготовленный набор инструкций в случае утечки или аварии"
+    description:
+      "У вас химическое предприятие с большой территорией? Запустите, оснащенного необходимым набором сенсоров, робота контролировать периметр и он выполнит подготовленный набор инструкций в случае утечки или аварии",
   },
   {
     id: 5,
     icon: icons5,
     title: "Разминирование",
-    description: "Дистанционное управление по радио или кабелю позволяет исключить риски при столь опасной работе, а бесчисленный список доп. оснащения еще и ускорит этот процесс"
+    description:
+      "Дистанционное управление по радио или кабелю позволяет исключить риски при столь опасной работе, а бесчисленный список доп. оснащения еще и ускорит этот процесс",
   },
   {
     id: 6,
     icon: icons6,
     title: "Доставка грузов",
-    description: "Роботы позволят автоматизировать маршрут доставки ваших грузов или избавят человека от физической работы при ручном управлении"
+    description:
+      "Роботы позволят автоматизировать маршрут доставки ваших грузов или избавят человека от физической работы при ручном управлении",
   },
   {
     id: 7,
     icon: icons7,
     title: "Поисково-спасательные операции",
-    description: "Агрессивная среда не станет преградой для робота при совершении спасательных миссий"
+    description:
+      "Агрессивная среда не станет преградой для робота при совершении спасательных миссий",
   },
   {
     id: 8,
     icon: icons8,
     title: "Создание фотореалистичных 3D карт местности",
-    description: "Есть задача построить точную 3д карту местности, туннеля или шахты? Робот сможет сделать это в автоматическом режиме без перерыва на обед или исключить риски здоровью оператору при ручном управлении в небезопасных местах"
-  }
+    description:
+      "Есть задача построить точную 3д карту местности, туннеля или шахты? Робот сможет сделать это в автоматическом режиме без перерыва на обед или исключить риски здоровью оператору при ручном управлении в небезопасных местах",
+  },
 ];
 
 const MobileWidget = ({ application, isLeft }) => (
-   <div className={isLeft ? classes.widget_left : classes.widget_right}>
-     {isLeft ? (
-        <>
-          <img className={classes.img} src={application.icon} alt="" />
-          <div className={classes.widgets_title_left}>
-            <h2>{application.title}</h2>
-            <p>{application.description}</p>
-          </div>
-        </>
-     ) : (
-        <>
-          <div className={classes.widgets_title_right}>
-            <h2>{application.title}</h2>
-            <p>{application.description}</p>
-          </div>
-          <img className={classes.img} src={application.icon} alt="" />
-        </>
-     )}
-   </div>
+  <div className={isLeft ? classes.widget_left : classes.widget_right}>
+    {isLeft ? (
+      <>
+        <img className={classes.img} src={application.icon} alt="" />
+        <div className={classes.widgets_title_left}>
+          <h2>{application.title}</h2>
+          <p>{application.description}</p>
+        </div>
+      </>
+    ) : (
+      <>
+        <div className={classes.widgets_title_right}>
+          <h2>{application.title}</h2>
+          <p>{application.description}</p>
+        </div>
+        <img className={classes.img} src={application.icon} alt="" />
+      </>
+    )}
+  </div>
 );
 
-// Компонент для десктопной иконки
-const DesktopIcon = ({ application, hoverContentClass = classes.hover_content }) => (
-   <div className={classes.icons_block}>
-     <img className={`${classes.icon_img} ${classes.initial_icon}`} src={application.icon} alt={application.title} />
-     <div className={hoverContentClass}>
-       <div className={classes.icon_wrapper}>
-         <img className={classes.icon_img} src={application.icon} alt={application.title} />
-         <h3>{application.title}</h3>
-       </div>
-       <p>{application.description}</p>
-     </div>
-   </div>
+const DesktopIcon = ({
+  application,
+  index,
+  onHover,
+  onLeave,
+  hoverContentClass = classes.hover_content,
+}) => (
+  <div
+    className={classes.icons_block}
+    onMouseEnter={() => onHover(index)}
+    onMouseLeave={onLeave}
+  >
+    <img
+      className={`${classes.icon_img} ${classes.initial_icon}`}
+      src={application.icon}
+      alt={application.title}
+    />
+    <div className={hoverContentClass}>
+      <div className={classes.icon_wrapper}>
+        <img
+          className={classes.icon_img}
+          src={application.icon}
+          alt={application.title}
+        />
+        <h3>{application.title}</h3>
+      </div>
+      <p>{application.description}</p>
+    </div>
+  </div>
 );
 
 const ScopeOfApplication = () => {
   const [isMobile, setIsMobile] = useState(false);
+  const [hoveredIndex, setHoveredIndex] = useState(null);
 
   useEffect(() => {
     const mediaQuery = window.matchMedia("(max-width: 1024px)");
@@ -113,66 +150,112 @@ const ScopeOfApplication = () => {
   }, []);
 
   const renderMobileView = () => (
-     <div className={classes.mobileContainer}>
-       <img src={mobileRobot} alt="Robot" />
-       <div className={classes.block_widgets}>
-         {applications.map((app, index) => (
-            <MobileWidget
-               key={app.id}
-               application={app}
-               isLeft={index % 2 === 0}
-            />
-         ))}
-       </div>
-     </div>
+    <div className={classes.mobileContainer}>
+      <img src={mobileRobot} alt="Robot" />
+      <div className={classes.block_widgets}>
+        {applications.map((app, index) => (
+          <MobileWidget
+            key={app.id}
+            application={app}
+            isLeft={index % 2 === 0}
+          />
+        ))}
+      </div>
+    </div>
   );
 
   const renderDesktopView = () => (
-     <div className={classes.main_block}>
-       <div className={classes.layout_container}>
-         {/* Верхний ряд */}
-         <div className={classes.top_row}>
-           <DesktopIcon application={applications[0]} />
-           <DesktopIcon application={applications[1]} />
-         </div>
+    <div className={classes.main_block}>
+      <div className={classes.layout_container}>
+        {/* Верхний ряд */}
+        <div className={classes.top_row}>
+          <DesktopIcon
+            application={applications[0]}
+            index={0}
+            onHover={setHoveredIndex}
+            onLeave={() => setHoveredIndex(null)}
+          />
+          <DesktopIcon
+            application={applications[1]}
+            index={1}
+            onHover={setHoveredIndex}
+            onLeave={() => setHoveredIndex(null)}
+          />
+        </div>
 
-         {/* Средняя секция */}
-         <div className={classes.middle_section}>
-           <div className={classes.left_side}>
-             <DesktopIcon application={applications[2]} hoverContentClass={classes.hover_content_left} />
-             <DesktopIcon application={applications[4]} hoverContentClass={classes.hover_content_left} />
-           </div>
+        {/* Средняя секция */}
+        <div className={classes.middle_section}>
+          <div className={classes.left_side}>
+            <DesktopIcon
+              application={applications[2]}
+              index={2}
+              onHover={setHoveredIndex}
+              onLeave={() => setHoveredIndex(null)}
+              hoverContentClass={classes.hover_content_left}
+            />
+            <DesktopIcon
+              application={applications[4]}
+              index={4}
+              onHover={setHoveredIndex}
+              onLeave={() => setHoveredIndex(null)}
+              hoverContentClass={classes.hover_content_left}
+            />
+          </div>
 
-           <div className={classes.duble_images}>
-             <img src={background2} alt="Robot" />
-           </div>
+          <div className={classes.duble_images}>
+            <img
+              src={images[hoveredIndex] || background2}
+              alt="Central"
+              className={classes.scopeImage}
+            />
+          </div>
 
-           <div className={classes.right_side}>
-             <DesktopIcon application={applications[3]} />
-             <DesktopIcon application={applications[5]} />
-           </div>
-         </div>
+          <div className={classes.right_side}>
+            <DesktopIcon
+              application={applications[3]}
+              index={3}
+              onHover={setHoveredIndex}
+              onLeave={() => setHoveredIndex(null)}
+            />
+            <DesktopIcon
+              application={applications[5]}
+              index={5}
+              onHover={setHoveredIndex}
+              onLeave={() => setHoveredIndex(null)}
+            />
+          </div>
+        </div>
 
-         {/* Нижний ряд */}
-         <div className={classes.bottom_row}>
-           <DesktopIcon application={applications[6]} />
-           <DesktopIcon application={applications[7]} />
-         </div>
-       </div>
-     </div>
+        {/* Нижний ряд */}
+        <div className={classes.bottom_row}>
+          <DesktopIcon
+            application={applications[6]}
+            index={6}
+            onHover={setHoveredIndex}
+            onLeave={() => setHoveredIndex(null)}
+          />
+          <DesktopIcon
+            application={applications[7]}
+            index={7}
+            onHover={setHoveredIndex}
+            onLeave={() => setHoveredIndex(null)}
+          />
+        </div>
+      </div>
+    </div>
   );
 
   return (
-     <>
-       <main className={classes.main}>
-         <div className={classes.main_title_block}>
-           <h2>Сферы применения</h2>
-           <p>В каких областях мы можем помочь</p>
-         </div>
-         {isMobile ? renderMobileView() : renderDesktopView()}
-       </main>
-       <Features />
-     </>
+    <>
+      <main className={classes.main}>
+        <div className={classes.main_title_block}>
+          <h2>Сферы применения</h2>
+          <p>В каких областях мы можем помочь</p>
+        </div>
+        {isMobile ? renderMobileView() : renderDesktopView()}
+      </main>
+      <Features />
+    </>
   );
 };
 
